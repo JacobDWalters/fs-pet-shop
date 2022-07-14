1dc486cf94a5197c2d021a787a451fdd0d46bd28
JacobDWalters <99148047+JacobDWalters@users.noreply.github.com>
2022-07-14 09:51:06 -0600

    All keywords functions operate/ no promises  Please enter the commit message for your changes. Lines starting

--- pets.js
+++ pets.js
@@ -1,3 +1,4 @@
+
 //gives acces to the fs commands to access files
 import fs from 'fs';
 
@@ -26,6 +27,8 @@ switch(subcommand) {
             const data = JSON.parse(str);
 
             // log a message if the index is not one of the options
+            if (!selector) console.log(data);
+            
             if (data[selector] == undefined || selector < 0) {
                 console.error('Usage: node pets.js read INDEX');
             } else {
@@ -49,29 +52,24 @@ switch(subcommand) {
 
             // create your pet if all the inputs are there
             if(age && kind && name) {
-                let input = {
+                let newPet = {
                     "age": age,
                     "kind": kind,
                     "name": name
                 };
 
-                data.push(input);
+                // display your new pet and add it to your list
+                console.log(newPet);
+                data.push(newPet);
 
                 // put your pet into json acceptable format
-                let inputJSON = JSON.stringify(data);
+                let dataJSON = JSON.stringify(data);
 
                 // write a file with your new pet 
-                fs.writeFile('pets.json', inputJSON, (error) => {
+                fs.writeFile('pets.json', dataJSON, (error) => {
                     if (error) process.exit(1);
+                });
 
-                    // as a callback function read your new file
-                    fs.readFile('pets.json', 'utf-8', (error, str) => {
-                    if (error) process.exit(1);
-
-                    const result = JSON.parse(str);
-                        console.log(result[result.length - 1]);
-                    })
-                })
             // log a message if all the inputs are not present
             } else {
                 console.error('Usage: node pets.js create AGE KIND NAME');
@@ -83,11 +81,69 @@ switch(subcommand) {
 
     // if case is input
     case 'update': {
+        fs.readFile('./pets.json', 'utf-8', (error, str) => {
+            if (error) process.exit(1);
+            var data = JSON.parse(str);
+    
+            // grab the inputs you need to update
+            const index = process.argv[3]
+            const age = parseInt(process.argv[4]);
+            const kind = process.argv[5];
+            const name = process.argv[6];
+
+            // update your pet if all the inputs are there
+            if(index && age && kind && name) {
+                let updatePet = data[index]
+                updatePet.age = age;
+                updatePet.kind = kind;
+                updatePet.name = name;
+
+                console.log(updatePet);
+
+                // put your pet into json acceptable format
+                let dataJSON = JSON.stringify(data);
+
+                // write a file with your new pet 
+                fs.writeFile('pets.json', dataJSON, (error) => {
+                    if (error) process.exit(1);
+                });
+            // log a message if all the inputs are not present
+            } else {
+                console.error('Usage: node pets.js update INDEX AGE KIND NAME');
+            }
+        });
         break;
     }
 
     // if destroy is input
     case 'destroy': {
+        fs.readFile('./pets.json', 'utf-8', (error, str) => {
+            if (error) process.exit(1);
+            var data = JSON.parse(str);
+    
+            // grab the input you want to remove
+            const index = process.argv[3]
+
+            // update your pet if all the inputs are there
+            if(index) {
+                // show the pet that you are removing
+                console.log(data[index]);
+                
+                // remove the pet from the list
+                data.splice(index);
+                
+                // put your list back into JSON format
+                let dataJSON = JSON.stringify(data);
+
+                // write a file without your pet
+                fs.writeFile('pets.json', dataJSON, (error) => {
+                    if (error) process.exit(1);
+                });
+            // log a message if all the inputs are not present
+            } else {
+                console.error('Usage: node pets.js destroy INDEX');
+            }
+        });
         break;
     }
 
