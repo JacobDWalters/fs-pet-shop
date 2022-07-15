4b38cfe90f6de2a916434e1bce04777507f32f3a
JacobDWalters <99148047+JacobDWalters@users.noreply.github.com>
2022-07-15 16:24:38 -0600

    create function added. using promises for express

--- expressServer.js
+++ expressServer.js
@@ -1,58 +1,57 @@
-import express from 'express';
-import fs from 'fs';
+import express, { application } from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 const PORT = 3000;
-const petRegExp = /^\/pets\/(.*)$/;
+// const readUrl = "/pets"
+// const readIndexUrl = "/pets/:index"
 
-const filter = function (req, res, next) {
-    const match = req.url.match(petRegExp);
+// const filter = function (req, res, next) {
+//     if (req.url !== readUrl || req.url !==readIndexUrl) res.sendStatus(404);
+//     else next();
+// }
 
-    if(match) next();
-    else res.send("invalid input");
-    
-}
-
-app.use(filter);
-// app.use(express.json);
+// app.use(filter);
+app.use(express.json());
 
 // Get request to handle the whole list
-app.get((req, res) => {
-    fs.readFile("./pets.json", "utf-8", (err, str) => {
-        if (err) process.exit(1);
-        
-        const index = match[1];
-        const petsList = JSON.parse(str);
-        const pet = petsList[index];
-        res.send(pet);
+app.get("/pets", (req, res) => {
+    fs.readFile("./pets.json", "utf-8").then((str) => {
+        const data = JSON.parse(str);
+        res.send(data);
+    });  
+});
+
+app.get("/pets/:index", (req, res) => {
+    fs.readFile("./pets.json", "utf-8").then((str) => {
+        const data = JSON.parse(str);
+        const index = req.params.index;
+
+        if(data[index]) res.send(data[index]);
+        else {
+            res.status(404).set('Content-Type', 'text-plain').send();
+        }
     });
 });
 
+// create function responding to a post request
+app.post("/pets", (req, res) => {
+    fs.readFile("./pets.json", "utf-8").then((str) => {
+        const data = JSON.parse(str);
+        const newPet = req.body;
 
-// // create function responding to a post request
-// app.post("/pets/:age/:type:/name", (req, res) => {
-//     fs.readFile("pets.json", "utf-8", (err, str) => {
-//         if (error) process.exit(1);
-
-//         const petsList = JSON.parse(str);
-//         const newPet = req.params;
-//         newPet.age = parseInt(newPet.age);
-//         petsList.push(newPet);
-//         res.send(newPet);
-//         fs.writeFile("pets.json", JSON.stringify(petsList), (err) => {
-//             console.log("success"); 
-//         });
-//     });
-// });
-
-
-// Return 200
-// Content-Type is application/json
-// Return all pets as JSON
-
-
-
-
+        if (!newPet.age || !newPet.kind || !newPet.name) {
+            res.status(400).set('content-Type', 'text/plain').send();
+        } else if (typeof(newPet.age) !== 'number') {
+            res.status(400).set('content-Type', 'text/plain').send();
+        } else {
+            data.push(newPet);
+            return fs.writeFile("./pets.json", JSON.stringify(data)).then(() => {
+                res.send(newPet);
+            });
+        }
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
